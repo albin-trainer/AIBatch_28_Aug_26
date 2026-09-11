82b16e299b65d0c5f24b6a54b3f06e92eae3f5b7
unknown <albinxavier@gmail.com>
2026-09-11 13:02:29 +0530

    Chat inmemory

---
+++ Spring AI Projects/chat-in-memory/src/main/java/com/example/AIController.java
@@ -0,0 +1,44 @@
+package com.example;
+import java.util.List;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AIController {
+       private ChatClient chatClient;
+       private 	ChatMemory chatMemory ;
+      public AIController(ChatClient.Builder builder ) {
+     		 chatMemory =
+			    MessageWindowChatMemory.builder()
+			        .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                    .maxMessages(10)
+			        .build();
+		this.chatClient = builder
+			    .defaultAdvisors(
+			        MessageChatMemoryAdvisor.builder(chatMemory).build()
+			    )
+			    .build();
+    }
+    @GetMapping("/history/{uname}")
+    public List<Message> getHistory(@PathVariable String uname) {
+    return chatMemory.get(uname);
+}
+    @GetMapping("/chat")
+    public String chat(@RequestParam String prompt, @RequestParam String uname) {               
+        return chatClient.prompt()
+                .user(prompt)
+                  .advisors(advisor -> advisor.param( ChatMemory.CONVERSATION_ID,
+			        uname
+			    ))
+                .call()
+                .content();
+    }
+
+   
+}
