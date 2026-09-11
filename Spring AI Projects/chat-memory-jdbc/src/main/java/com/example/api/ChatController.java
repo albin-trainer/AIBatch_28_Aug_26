0601f4601431250a56c780adc85f412a10c99881
unknown <albinxavier@gmail.com>
2026-09-11 15:48:06 +0530

    Chat memory JDBC

---
+++ Spring AI Projects/chat-memory-jdbc/src/main/java/com/example/api/ChatController.java
@@ -0,0 +1,41 @@
+package com.example.api;
+
+import java.util.List;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+	private final ChatClient chatClient;
+	private final ChatMemory chatMemory;
+	 public ChatController(ChatClient chatClient, ChatMemory chatMemory) {
+	        this.chatClient = chatClient;
+			this.chatMemory=chatMemory;
+	    }
+
+	    @GetMapping("/chat")
+	    public String chat(@RequestParam String uname,
+	                       @RequestParam String message) {
+
+	        return chatClient.prompt()
+	                .user(message)
+	                .advisors(a -> a.param(
+	                        ChatMemory.CONVERSATION_ID,
+	                        uname))
+	                .call()
+	                .content();
+	    }
+		    @GetMapping("/history/{uname}")
+public List<Message> getHistory(@PathVariable String uname) {
+    return chatMemory.get(uname);
+}
+
+}
